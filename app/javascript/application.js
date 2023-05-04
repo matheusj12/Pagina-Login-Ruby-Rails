cfc3d7f2ff23701c3d7a1e3591b9f72732a6eb62
matheusj12 <matheusjulio12@hotmail.com>
2023-05-03 23:00:02 -0300

    Login Screen Improvement And Styling

--- app/javascript/application.js
+++
@@ -1,9 +0,0 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
-import "controllers"
-import "~bootstrap/scss/bootstrap";
-import url('https://fonts.googleapis.com/css?family=Merriweather:400,700');
-//= require jquery3
-//= require popper
-//= require bootstrap-sprockets
-//= link_tree ../node_modules/bootstrap/dist/js/ .js
