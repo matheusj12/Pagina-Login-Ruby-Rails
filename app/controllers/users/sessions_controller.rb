3d1a95478f21301e8904f3e7ccd86538dd53fc3b
matheusj12 <matheusjulio12@hotmail.com>
2023-06-19 03:15:36 -0300

    [Enhancement]enhancement/update_login_sing_out

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -17,15 +17,14 @@ class Users::SessionsController < Devise::SessionsController
     respond_with resource, location: after_sign_in_path_for(resource)
   end
 
-
   # DELETE /resource/sign_out
   def destroy
+    expire_fragment('views/*')
     signed_out = (Devise.sign_out_all_scopes ? sign_out : sign_out(resource_name))
     set_flash_message! :notice, :signed_out if signed_out
     yield if block_given?
     respond_to_on_destroy
   end
-
 
   # protected
 
