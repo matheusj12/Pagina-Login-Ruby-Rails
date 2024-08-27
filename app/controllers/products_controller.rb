4c2274c12b04f0f7d44f360dfda9e5e9fcb35c52
matheusj12 <matheusjulio12@hotmail.com>
2024-08-27 17:52:38 -0300

    new_venda_atu

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,5 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: %i[ show edit update destroy ]
+  before_action :set_product, only: %i[show edit update destroy]
 
   # GET /products or /products.json
   def index
@@ -57,14 +57,21 @@ class ProductsController < ApplicationController
     end
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_product
-      @product = Product.find(params[:id])
-    end
+  # GET /products/search?q=query
+  def search
+    @products = Product.where('name LIKE ?', "%#{params[:q]}%")
+    render json: @products
+  end
 
-    # Only allow a list of trusted parameters through.
-    def product_params
-      params.require(:product).permit(:name, :price, :description)
-    end
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_product
+    @product = Product.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def product_params
+    params.require(:product).permit(:name, :price, :description)
+  end
 end
--- app/models/venda.rb
+++ app/models/venda.rb
@@ -1,5 +1,14 @@
 class Venda < ApplicationRecord
-  belongs_to :client
+  has_many :order_items
+  accepts_nested_attributes_for :order_items
+  has_many :order_items, dependent: :destroy
+
+  validates :funcionario_id, :client_id, :data, presence: true
+end
+
+class OrderItem < ApplicationRecord
+  belongs_to :venda
   belongs_to :product
 
+  validates :product_id, :quantidade, presence: true
 end
